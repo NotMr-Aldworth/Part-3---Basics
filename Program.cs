399b84d5049956d6360dee5060f595d50d30ec00
Jackson Cowan <CowaJack474@GoTVDSB.ca>
2022-03-01 10:58:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_3___Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response;
+            
+            Console.WriteLine("What is your favourite Number?!");
+            response = Console.ReadLine();
+            Convert.ToInt32(response);
+            Console.WriteLine($"Your favorite number is  {response} ");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
